7ae2c8464d30ec94a041250b83b332e3b3ee5aaf
NaZe0320 <rwt1226@naver.com>
2024-12-17 17:52:47 +0900

    [init] 색상 + 그라디언트 테마 추가

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Brand Colors
+  static const Color primaryBlue = Color(0xFF2196F3);
+  static const Color secondaryBlue = Color(0xFFE3F2FD);
+  static const Color tertiaryBlue = Color(0xFF1565C0);
+
+  // Gray Scale
+  static const Color gray800 = Color(0xFF27272A);
+  static const Color gray700 = Color(0xFF3F3F46);
+  static const Color gray600 = Color(0xFF52525B);
+
+  // Background Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color surface = Color(0xFFF8FAFC);
+
+  // Border Colors
+  static const Color defaultBorder = Color(0xFFE4E4E7);
+
+  // Text Colors
+  static const Color primaryText = Color(0xFF1E293B);
+  static const Color secondaryText = Color(0xFF64748B);
+  static const Color pointWhite = Color(0xFFFFFFFF);
+
+  // State Colors
+  static const Color successBackground = Color(0xFFDCFCE7);
+  static const Color successText = Color(0xFF16A34A);
+  static const Color errorBackground = Color(0xFFFEE2E2);
+  static const Color errorText = Color(0xFFDC2626);
+  static const Color warningBackground = Color(0xFFFEF3C7);
+  static const Color warningText = Color(0xFFD97706);
+
+  // Achievement Colors
+  static const Color platinumText = Color(0xFF00261C);
+  static const Color goldText = Color(0xFF422006);
+  static const Color silverText = Color(0xFF1F2937);
+  static const Color bronzeText = Color(0xFFFFFFFF);
+}
---
+++ lib/core/theme/app_gradients.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  static const LinearGradient premiumGradient = LinearGradient(
+    colors: [Color(0xFFF59E0B), Color(0xFFF97316)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient premiumBlueGradient = LinearGradient(
+    colors: [Color(0xFF2196F3), Color(0xFF1565C0)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient actionGradient = LinearGradient(
+    colors: [Color(0xFF22C55E), Color(0xFF16A34A)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient achievementPlatinum = LinearGradient(
+    colors: [Color(0xFF34D399), Color(0xFF059669)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient achievementGold = LinearGradient(
+    colors: [Color(0xFFFCD34D), Color(0xFFF59E0B)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient achievementSilver = LinearGradient(
+    colors: [Color(0xFFE5E7EB), Color(0xFF9CA3AF)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static const LinearGradient achievementBronze = LinearGradient(
+    colors: [Color(0xFFD69E72), Color(0xFF93482F)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
