aa32e21d319e55a5520c8988bcdb6ee48a07e10a
NaZe0320 <rwt1226@naver.com>
2024-12-24 18:29:58 +0900

    [feat] 베이스 ViewModel 구현

--- lib/core/base/base_view_model.dart
+++ lib/core/base/base_view_model.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:typing_talk/core/base/base_intent.dart';
 
 abstract class BaseViewModel<I extends BaseIntent, S> extends StateNotifier<S> {
-  BaseViewModel(S state) : super(state);
+  BaseViewModel(super.state);
 
   void dispatch(I intent);
 }
