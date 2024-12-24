a3b7ec1138fa69046ac64eccf8bf1b82e62bd7f9
NaZe0320 <rwt1226@naver.com>
2024-12-24 18:29:15 +0900

    [feat] 상수 설정

---
+++ lib/core/constants/app_constant.dart
@@ -0,0 +1,9 @@
+class AppConstant {
+  AppConstant._();
+
+  //App Information
+  static const String appName = 'TypingTalk';
+  static const String appNameKR = '타이핑톡';
+  static const String appVersion = '1.0.0';
+  static const String appBuildNumber = '1';
+}
