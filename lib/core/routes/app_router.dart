21a1f950ffd9e1e3b517d06ee1c8074b6b890879
NaZe0320 <rwt1226@naver.com>
2024-12-27 01:41:54 +0900

    [feat] GoRouter 설정

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:typing_talk/presentation/features/home/screens/home_screen.dart';
+import 'package:typing_talk/presentation/features/practice/screens/practice_screen.dart';
+
+part 'app_router.g.dart';
+
+@riverpod
+GoRouter router(Ref ref) {
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'home',
+        builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: '/practice',
+        name: 'practice',
+        builder: (context, state) {
+          return PracticeScreen();
+        },
+      ),
+    ],
+    errorBuilder: (context, state) => Scaffold(
+      body: Center(
+        child: Text('Error: ${state.error}'),
+      ),
+    ),
+  );
+}
